7ccfb2471f532ddb562bfbe57851b7de4dc1a059
Kaji Ryoji <github@mail.kaji-ryoji.com>
2021-12-21 14:38:41 +0800

    chore!: move App location
    Signed-off-by: Kaji Ryoji <github@mail.kaji-ryoji.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@/styles/index.scss';
-import App from '@/containers/App';
+import App from '@/App';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
       <App />
-    </React.StrictMode>,
     </BrowserRouter>,
     document.getElementById('root'),
     () => {
