e6951cf5a915af08050d326a07bd1016bd95a759
Kaji Ryoji <github@mail.kaji-ryoji.com>
2021-12-21 14:39:01 +0800

    feat: add reg, auth and login
    Signed-off-by: Kaji Ryoji <github@mail.kaji-ryoji.com>

---
+++ src/services/auth.service.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const API_URL = "http://412505r54f.imdo.co/naivechair/author/";
+
+class AuthService {
+  login(username: string, password: string) {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password
+      })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username: string, email: string, password: string) {
+    return axios.post(API_URL + "signup", {
+      username,
+      password,
+      email
+    });
+  }
+
+  getCurrentUser() {
+    const userStr = localStorage.getItem("user");
+    if (userStr) return JSON.parse(userStr);
+
+    return null;
+  }
+}
+
+export default new AuthService();
